207669c7ac5a704950fe1c9222ad2891095deb29
roong <roongsetfire@gmail.com>
2021-03-23 13:53:38 +0700

    First Commit

---
+++ src/Comment.java
@@ -0,0 +1,22 @@
+
+class Comment {
+	public static void main(String[] args) {
+		/* Data type
+	Variable assign
+		 */
+		// Assign value
+		int myInt = 20;
+		boolean myBool = true;
+		String myString = "JAVA";
+		float myFloat = 1.99f;
+		char myChar = 'A';
+		System.out.println("This is a String "+myInt);
+		System.out.println("This is a String "+myBool);
+		System.out.println("This is a String "+myString);
+		System.out.println("This is a String "+myFloat);
+		System.out.println("This is a String "+myChar);
+		
+		
+
+	}
+}
